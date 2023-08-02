d8b9e84d37ad2ff7e13c95ffcf208dcb87aa730f
larakoseff <larakoseff@gmail.com>
2023-08-02 11:11:32 +0200

    Add Challenge 2 files

---
+++ challenge2/alex.js
@@ -0,0 +1,8 @@
+// alex.js
+
+const private firstname = "Alex"
+const private surname = "Naidoo"
+const public role = "Head of Marketing"
+
+const private display= firstname + " " + surname + " (" + role + ")"
+document.querySelector('#alex').innerText = display
